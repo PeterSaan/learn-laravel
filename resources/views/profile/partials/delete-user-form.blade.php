dae2c336b06a1e754bda8e8ce6d7f2c76d1b4f3c
PeterSaan <saanpeter@gmail.com>
2024-12-17 10:58:23 +0200

    profile done

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -11,48 +11,56 @@
                 </p>
             </header>
 
-            <div class="flex justify-end items-center">
-                <button
-                class="btn btn-secondary"
-                x-data=""
-                x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">
+            <div class="flex justify-end items-center mt-2">
+                <button class="btn btn-secondary" onclick="deletion_form.showModal()">
                     Delete account
                 </button>
             </div>
 
-            <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
-                <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
-                    @csrf
-                    @method('delete')
-
-                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            <dialog id="deletion_form" name="confirm-user-deletion" :show="$errors - > userDeletion - > isNotEmpty()"
+                class="modal modal-bottom sm:modal-middle">
+                <div class="modal-box">
+                    <h2 class="text-lg font-bold">
                         Are you sure you want to delete your account?
                     </h2>
 
-                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                    <p class="py-4 text-sm">
                         Once your account is deleted, all of its resources and data will be permanently deleted
                     </p>
+                    <div class="">
+                        <form method="post" action="{{ route('profile.destroy') }}" class="p-6 mt-6">
+                            @csrf
+                            @method('delete')
 
-                    <div class="mt-6">
-                        <x-input-label for="password" value="Password" class="sr-only" />
 
-                        <x-text-input id="password" name="password" type="password" class="mt-1 block w-3/4"
-                            placeholder="Password" />
 
-                        <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+                            <label class="form-control w-full">
+                                <div class="label">
+                                    <span class="label-text">Confirm your password</span>
+
+                                </div>
+                                <input name="conf-pass" type="password" placeholder="Your current password"
+                                    class="input input-bordered @error('conf-pass') input-error @enderror w-full" />
+                                <div class="label">
+                                    @error('conf-pass')
+                                        <span class="label-text-alt text-error">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </label>
+
+                            <div class="flex justify-between items-center my-4">
+                                <button type="button" class="btn btn-neutral" onclick="deletion_form.close()">
+                                    Cancel
+                                </button>
+                                <button class="btn btn-secondary">
+                                    Delete account
+                                </button>
+                            </div>
+
+                        </form>
                     </div>
-
-                    <div class="mt-6 flex justify-end">
-                        <x-secondary-button x-on:click="$dispatch('close')">
-                            Cancel
-                        </x-secondary-button>
-
-                        <x-danger-button class="ms-3">
-                            Delete account
-                        </x-danger-button>
-                    </div>
-                </form>
-            </x-modal>
+                </div>
+            </dialog>
         </div>
     </div>
 </section>
